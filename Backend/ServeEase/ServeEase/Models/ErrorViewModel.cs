3799f08020ea15e9d8625491429fa693da0ad4d2
Ayush Raut <ayushraut818@gmail.com>
2025-02-15 10:27:30 +0530

    Intialized ASP.NET Project for Backend

--- Backend/ServeEase/ServeEase/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ServeEase.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
