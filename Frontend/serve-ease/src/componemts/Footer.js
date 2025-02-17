fc444ae3c67ccc84fe978c6e801039200ffdea74
Chinmayee Hankare <chinuhankare@gmail.com>
2025-02-17 16:18:58 +0530

    Added footer file

---
+++ Frontend/serve-ease/src/componemts/Footer.js
@@ -0,0 +1,124 @@
+import React from 'react';
+import './Footer.css';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+      <section className='footer-subscription'>
+        <p className='footer-subscription-heading'>
+          Join the ServEase newsletter to receive our best deals
+        </p>
+        <p className='footer-subscription-text'>
+          You can unsubscribe at any time.
+        </p>
+        <div className='input-areas'>
+          <form>
+            <input
+              className='footer-input'
+              name='email'
+              type='email'
+              placeholder='Your Email'
+            />
+            <Button buttonStyle='btn--outline'>Subscribe</Button>
+          </form>
+        </div>
+      </section>
+      <div class='footer-links'>
+        <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+            <h2>About Us</h2>
+            <Link to='/sign-up'>How it works</Link>
+            <Link to='/'>Testimonials</Link>
+            <Link to='/'>Careers</Link>
+            <Link to='/'>Investors</Link>
+            <Link to='/'>Terms of Service</Link>
+          </div>
+          <div class='footer-link-items'>
+            <h2>Contact Us</h2>
+            <Link to='/'>Contact</Link>
+            <Link to='/'>Support</Link>
+            <Link to='/'>Destinations</Link>
+            <Link to='/'>Sponsorships</Link>
+          </div>
+        </div>
+        <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+            <h2>Videos</h2>
+            <Link to='/'>Submit Video</Link>
+            <Link to='/'>Ambassadors</Link>
+            <Link to='/'>Agency</Link>
+            <Link to='/'>Influencer</Link>
+          </div>
+          <div class='footer-link-items'>
+            <h2>Social Media</h2>
+            <Link to='/'>Instagram</Link>
+            <Link to='/'>Facebook</Link>
+            <Link to='/'>Youtube</Link>
+            <Link to='/'>Twitter</Link>
+          </div>
+        </div>
+      </div>
+      <section class='social-media'>
+        <div class='social-media-wrap'>
+          <div class='footer-logo'>
+            <Link to='/' className='social-logo'>
+              ServeEase
+              <i class='fab fa-typo3' />
+            </Link>
+          </div>
+          <small class='website-rights'>ServeEase © 2023</small>
+          <div class='social-icons'>
+            <Link
+              class='social-icon-link facebook'
+              to='/'
+              target='_blank'
+              aria-label='Facebook'
+            >
+              <i class='fab fa-facebook-f' />
+            </Link>
+            <Link
+              class='social-icon-link instagram'
+              to='/'
+              target='_blank'
+              aria-label='Instagram'
+            >
+              <i class='fab fa-instagram' />
+            </Link>
+            <Link
+              class='social-icon-link youtube'
+              to='/'
+              target='_blank'
+              aria-label='Youtube'
+            >
+              <i class='fab fa-youtube' />
+            </Link>
+            <a
+                className='social-icon-link twitter'
+                href='https:///.com/'
+                target='_blank'
+                rel='noopener noreferrer'
+                aria-label='Twitter'
+                >
+                <i className='fab fa-twitter' />
+            </a>
+            <a
+                className='social-icon-link twitter'
+                href='https://twitter.com//?s=20'
+                target='_blank'
+                rel='noopener noreferrer'
+                aria-label='LinkedIn'
+                >
+                <i className='fab fa-linkedin' />
+            </a>
+
+
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Footer;
